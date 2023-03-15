b26e376cd511cf7b15873966a76bfacfcd53db78
MalMWare <malkelleymartin@gmail.com>
2023-03-14 20:00:14 -0700

    added SearchContext

--- src/components/search.js
+++ src/components/search.js
@@ -1,21 +1,15 @@
 import { useState } from 'react'
+import { useContext } from 'react'
+import { SearchContext } from '../context/SearchContext'
 
-function Search(props) {
-    let [searchTerm, setSearchTerm] = useState('')
+function Search() {
+    const {term, setSearch} = useContext(SearchContext)
 
-function handleChange (e) {
-    setSearchTerm(e.target.value)
-}
-
-function handleSubmit (e) {
-    e.preventDefault()
-    props.setSearch(searchTerm) 
-}
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Enter a search term here" value={searchTerm} onChange={handleChange}/>
-            <input type="submit" />
+        <form >
+            <input ref={term} type="text" placeholder="Enter a search term here" />
+            <input type="submit" onClick={e => setSearch(e, term.current.value)} />
         </form>
     )
 }
